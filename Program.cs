6dcd376d82acdf644cf523654803321043d619be
orxanone <recebovorxan3@gmail.com>
2023-05-18 11:07:52 +0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kerpic sayini daxil edin");
+            int N=int.Parse(Console.ReadLine());
+            int matches = 20 * N - 8;
+            Console.WriteLine("Lazim olan en az kibrit miqdari:"+matches);
+        }
+    }
+}
